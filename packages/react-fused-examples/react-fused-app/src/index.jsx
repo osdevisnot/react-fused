fe58521adda1bc81a5194b3d5532886b291bf474
osdevisnot <osdevisnot@gmail.com>
2017-08-06 22:58:18 -0700

    fix the imports

--- packages/react-fused-examples/react-fused-app/src/index.jsx
+++ packages/react-fused-examples/react-fused-app/src/index.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-console.log('this works');
-// babel plugin react-app causes this to fail.. needs investigation
-// ReactDOM.render(<div>Hello World !!</div>, document.getElementById('app'));
+ReactDOM.render(<div>Hello World !!</div>, document.getElementById('app'));
